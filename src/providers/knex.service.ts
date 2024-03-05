6456cb44ee4a433e31ed2182f554c442eff79da5
muhammadabdumalikov <burakali424@gmail.com>
2024-03-05 23:08:52 +0500

    add

--- src/providers/knex.service.ts
+++ src/providers/knex.service.ts
@@ -1,25 +1,3 @@
-// import knex, { Knex } from "knex";
-
-// export class KnexService {
-//   instance: Knex;
-
-//   constructor() {
-//     this.instance = knex({
-//       client: 'postgresql',
-//       connection: {
-//         host: 'localhost',
-//         database: 'horeca',
-//         password: '4324',
-//         user: 'postgres',
-//       },
-//       pool: {
-//         min: 2,
-//         max: 20,
-//       },
-//     })
-//   }
-// }
-
 import knex, { Knex } from "knex";
 
 export class KnexService {
